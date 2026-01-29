76f692ef0812ec761e3c5c3af0e8485aef05fec6
rifkyyy <rifky@192.168.1.4>
2026-01-29 18:56:29 +0700

    fix: downgrade nextjs for cloudflare compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // Pake ini, jangan 'tailwindcss' doang
+    'autoprefixer': {},
   },
 };
